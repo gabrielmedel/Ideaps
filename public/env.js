e2463f9983f5342655f57730a0a8971e33759233
Unequaly <unequalyofficial@gmail.com>
2021-03-28 01:28:27 +0100

    V.0.1

---
+++ public/env.js
@@ -0,0 +1,4 @@
+window.env = {
+  "API_URL": "https://ideaps-project.herokuapp.com/v1/graphql",
+  "DEFAULT_TOKEN": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYW5vbnltb3VzIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6ImFub255bW91cyIsIngtaGFzdXJhLXJvbGUiOiJhbm9ueW1vdXMifX0.LB60u48qYVms8Jj4SonPMEfwtZtSFn4wGOVll3dCyaI"
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import "./styles/App.css"
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import { Test } from "./Test"
 import { createHttpLink } from "@apollo/react-hooks"
 import { setContext } from "@apollo/client/link/context"
+import env from "react-dotenv"
 
 const httpLink = createHttpLink({
-  uri: "https://ideaps-project.herokuapp.com/v1/graphql",
+  uri: env.API_URL,
 })
+
+const defaulRole = env.DEFAULT_TOKEN
+
+const token = JSON.parse(localStorage.getItem("token"))
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYW5vbnltb3VzIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6ImFub255bW91cyIsIngtaGFzdXJhLXVzZXItaWQiOiIxMjM0NTY3ODkwIiwieC1oYXN1cmEtb3JnLWlkIjoiMTIzIiwieC1oYXN1cmEtY3VzdG9tIjoiMSJ9fQ.tUFhwyFQkyKIibB1JLn-giMLlVWIFwl_opRub7QDtqA`,
+      authorization: `${token}`,
     },
   }
 })
@@ -24,6 +29,11 @@ const client = new ApolloClient({
 })
 
 function App() {
+  const [logged] = useState(false)
+  !logged
+    ? localStorage.setItem("token", JSON.stringify(defaulRole))
+    : localStorage.setItem("token", JSON.stringify("defaulRole"))
+
   return (
     <div className="App">
       <ApolloProvider client={client}>
